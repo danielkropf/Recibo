100d28fcab75c92e788f54fafe9632980ef25933
danielkropf <danielkropf@uol.com.br>
2015-03-09 12:20:13 -0300

    Segunda Versão
    Função de Adicionar Limpar e Remover Prontas

---
+++ Recibos 2.0/Recibos/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Recibos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adicionar = new System.Windows.Forms.TextBox();
+            this.remover = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adicionar
+            // 
+            this.adicionar.Location = new System.Drawing.Point(20, 46);
+            this.adicionar.Name = "adicionar";
+            this.adicionar.Size = new System.Drawing.Size(198, 20);
+            this.adicionar.TabIndex = 1;
+            // 
+            // remover
+            // 
+            this.remover.Location = new System.Drawing.Point(20, 75);
+            this.remover.Name = "remover";
+            this.remover.Size = new System.Drawing.Size(198, 20);
+            this.remover.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(224, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.add);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remover";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.remove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "daniel",
+            "batata",
+            "dollynho",
+            "vai pikachu"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(20, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.WordWrap = false;
+            this.textBox1.Click += new System.EventHandler(this.aqaqa);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.clear);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 186);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.remover);
+            this.Controls.Add(this.adicionar);
+            this.Name = "Form1";
+            this.Text = "Recibo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox adicionar;
+        private System.Windows.Forms.TextBox remover;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ Recibos 2.0/Recibos/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recibos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void add(object sender, EventArgs e)
+        {
+            textBox1.AutoCompleteCustomSource.Add(adicionar.Text);
+            adicionar.Text = "";
+        }
+
+        private void remove(object sender, EventArgs e)
+        {
+            textBox1.AutoCompleteCustomSource.Remove(remover.Text);
+            remover.Text = "";
+        }
+
+        private void clear(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            remover.Text = "";
+            adicionar.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aqaqa(object sender, EventArgs e)
+        {
+
+        }            
+    }
+}
