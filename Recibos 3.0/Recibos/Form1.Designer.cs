856ee8920a0c1301d0989f6cb33b642b55636dad
danielkropf <danielkropf@uol.com.br>
2015-03-12 16:24:37 -0300

    Terceira Versão
    Mais organizada e com caixas de texto para valor e descrição do produto

---
+++ Recibos 3.0/Recibos/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Recibos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.valor = new System.Windows.Forms.TextBox();
+            this.descricao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.add);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(216, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Remover";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.remove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "daniel",
+            "batata",
+            "dollynho",
+            "vai pikachu"});
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.WordWrap = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(216, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.clear);
+            // 
+            // valor
+            // 
+            this.valor.Location = new System.Drawing.Point(12, 113);
+            this.valor.Name = "valor";
+            this.valor.Size = new System.Drawing.Size(75, 20);
+            this.valor.TabIndex = 7;
+            this.valor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // descricao
+            // 
+            this.descricao.AutoCompleteCustomSource.AddRange(new string[] {
+            "Cota Mensal",
+            "Cota Mensal Janeiro",
+            "Cota Mensal Fevereiro",
+            "Cota Mensal Março",
+            "Cota Mensal Abril",
+            "Cota Mensal Maio",
+            "Cota Mensal Junho",
+            "Cota Mensal Julho",
+            "Cota Mensal Agosto",
+            "Cota Mensal Setembro",
+            "Cota Mensal Outubro",
+            "Cota Mensal Novembro",
+            "Cota Mensal Dezembro",
+            "Uniforme",
+            "Agenda",
+            "Mochila",
+            "Matrícula",
+            "Entrada Anuidade",
+            "Balé",
+            "Judô",
+            "Livros Didáticos",
+            "Material de Inglês",
+            "Evento Dia das Mães",
+            "Evento Dia dos Pais",
+            "Semana das Crianças",
+            "Evento de Encerramento"});
+            this.descricao.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.descricao.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.descricao.Location = new System.Drawing.Point(93, 113);
+            this.descricao.Name = "descricao";
+            this.descricao.Size = new System.Drawing.Size(198, 20);
+            this.descricao.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Valor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Descrição";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(135, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Adicionar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.add2);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(216, 139);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Remover";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.remove2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 210);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.descricao);
+            this.Controls.Add(this.valor);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Recibo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox valor;
+        private System.Windows.Forms.TextBox descricao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+
+    }
+}
+
---
+++ Recibos 3.0/Recibos/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recibos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void add(object sender, EventArgs e)
+        {
+            textBox1.AutoCompleteCustomSource.Add(textBox1.Text);
+        }
+
+        private void remove(object sender, EventArgs e)
+        {
+            textBox1.AutoCompleteCustomSource.Remove(textBox1.Text);
+        }
+
+        private void clear(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            descricao.Text = "";
+            valor.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 08 && e.KeyChar != 127 && e.KeyChar != 44)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void add2(object sender, EventArgs e)
+        {
+            descricao.AutoCompleteCustomSource.Add(descricao.Text);
+        }
+
+        private void remove2(object sender, EventArgs e)
+        {
+            descricao.AutoCompleteCustomSource.Remove(descricao.Text);
+        }            
+    }
+}
